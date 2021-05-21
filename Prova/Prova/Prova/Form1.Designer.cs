115120095dc0418dbf407f771f7d7a7438e35a99
Clark Maltempi <68931244+MEhele@users.noreply.github.com>
2021-05-21 20:10:35 -0300

    Add files via upload

---
+++ Prova/Prova/Prova/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Prova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.ListaBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.ForeColor = System.Drawing.Color.Maroon;
+            this.btnExecutar.Location = new System.Drawing.Point(91, 144);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(93, 68);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Cadastro Vendas";
+            this.btnExecutar.UseVisualStyleBackColor = false;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // ListaBox
+            // 
+            this.ListaBox.FormattingEnabled = true;
+            this.ListaBox.Location = new System.Drawing.Point(245, 36);
+            this.ListaBox.Name = "ListaBox";
+            this.ListaBox.Size = new System.Drawing.Size(387, 277);
+            this.ListaBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(667, 354);
+            this.Controls.Add(this.ListaBox);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox ListaBox;
+    }
+}
+
---
+++ Prova/Prova/Prova/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace Prova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            double[,] Vendas = new double[9, 4];
+            double[] Meses = new double[9];
+            string preco;
+            double totalMeses = 0;
+            int cont = 0;
+            int i = 0;
+
+            for (cont = 0; cont < 9; cont++)
+            {   
+                //Verifica entrada de dados
+                for (i = 0; i < 4; i++)
+                {
+                    preco = Interaction.InputBox("Digite as Vendas da semana " + (i + 1) + " Do Mês: " + (cont + 1), "Entrada de dados");
+                    if (preco == "")
+                    {
+                        MessageBox.Show("Campo vazio");
+                        i--;
+                    } //verifica entrada de dados do tipo "double"
+                    else if (double.TryParse(preco, out Vendas[cont, i]))
+                    {
+                        double.TryParse(preco, out Vendas[cont, i]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Digite apenas números!");
+                        i--;
+                    }
+                }
+            }
+
+            //Faz o faturamento
+            for (cont = 0; cont < 9; cont++)
+            {
+                for (i = 0; i < 4; i++)
+                {
+                    Meses[cont] = Meses[cont] + Vendas[cont, i];
+                }
+                totalMeses = totalMeses + Meses[cont];
+            }
+
+            //Visualização 
+            for (cont = 0; cont < 9; cont++)
+            {
+                for (i = 0; i < 4; i++)
+                {
+                  ListaBox.Items.Add("Total do Mês: " + (cont + 1) + " Semana: " + (i + 1) + ": R$" + Vendas[cont, i].ToString("N2"));
+                }
+                ListaBox.Items.Add("*Total Mês: R$" + Meses[cont].ToString("N2"));
+                ListaBox.Items.Add("         ");
+            }
+                ListaBox.Items.Add("*Total Geral: R$" + totalMeses.ToString("N2"));
+
+
+        }
+    }
+
+}
+
+
