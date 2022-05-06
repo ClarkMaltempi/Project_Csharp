8539863e2b09fff29e295819c46a26fe95732ddf
Clark Maltempi <68931244+MEhele@users.noreply.github.com>
2022-05-06 16:36:04 -0300

    Update script.cs

--- script.cs
+++ script.cs
@@ -135,3 +135,95 @@ int idade = int.Parse(Console.ReadLine());
 //Função de Modulo que transforma numero negativo em positivo
 
 int precoAbs = Math.Abs(preco);
+
+//Arrays**********************************************
+
+string[] produtos = new string[5];
+string[] produtos = new string[5]{"Fifa", 
+			"Minecraft",
+			"Age of Empires",
+			"Dark Colony",
+			"MIB"
+			};
+			
+int[] valores = {40,50,60,70,20};
+
+//Switch********************************************
+	cor = "Azul";
+
+switch(cor){
+	
+	
+	case "Vermelho":
+	console.WhiteLine("Sua cor favorita é Vermelha");
+	break;
+	
+	case "Amarelo":
+	console.WhiteLine("Sua cor favorita é Amarelo");
+	break;
+	
+	case "Azul":
+	console.WhiteLine("Sua cor favorita é Amarelo");
+	break;
+	
+}
+
+//ENUM*********************************************Criar tipos de dados
+
+//Conjunto de dados limitado
+
+class Program
+{
+	
+	enum Cor{Azul = 33, Verde, Amarelo, Vermelho};//pode-se alterar o índice
+	enum Cor{Azul, Verde, Amarelo, Vermelho};
+	
+	
+	static void Main(string[] args)
+	{
+		Cor corFavorita = Cor.Vermelho;
+		Cor corFavoritaCarla = Cor.Azul;
+		
+		Console.WriteLine(corFavorita);
+		
+		Console.WriteLine((int)corFavorita);//converter o tipo "cast"
+		Console.WriteLine((Cor)2);//Convertendo inteiro 
+	}
+}
+
+//ENUM com Switch*****************************************************************************************Criar tipos de dados
+
+enum Opcao{Criar=1, Deletar, Editar, Listar, Atualizar};
+
+	Console.WriteLine("1-Criar\n, 2-Deletar\n, 3-Editar\n, 4-Listar\n, 5-Atualizar\n");
+	int index = int.Parse(Console.ReadLine());
+	
+	//Vai Pegar as opções do ENUM
+	Opcao opcaoSelecionada = (Opcao)index;
+	
+	switch(opcaoSelecionada)
+	{
+		case Opcao.Criar:
+		Console.WriteLine("Criar Algo");
+		break;
+		case Opcao.Deletar:
+		Console.WriteLine("Delete");
+		break;
+		case Opcao.Editar:
+		Console.WriteLine("Edite Algo");
+		break;
+	
+		default:
+		Console.WriteLine("Opção não encontrado");
+		break;
+	}
+	
+//While*****************************************************************************************
+
+int contador =0;
+while(contador < 10){
+	
+	Console.WhriteLine("Rodando o While!");
+	contador++;//contador += 1;
+}
+	Console.WhriteLine("FIM DA LINHA!");
